0ae6937261318ab6870280450f64a830e7f96875
Mark Mesander <mark_mesander@hotmail.com>
2023-09-14 15:08:05 +0200

    add Ingredient class and constructor

---
+++ src/Ingredient.java
@@ -0,0 +1,13 @@
+public class Ingredient {
+    String amount;
+    String unit;
+    String name;
+
+    public Ingredient(String amount, String unit, String name) {
+        this.amount = amount;
+        this.unit = unit;
+        this.name = name;
+    }
+
+
+}
